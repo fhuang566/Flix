4e8c1077e0a725e794c2b0107c7943b4b105078c
fhuang566 <fhuang566@gmail.com>
2021-09-20 19:09:30 -0700

    hw02

---
+++ flister/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flister
+//
+//  Created by Huang, Fenghai on 9/20/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
